0f8bd068ac6d4d9461eeec6b8f38c267dbace3d1
AidenBernstein <aiden@sheldonbernstein.com>
2022-09-03 21:47:00 -0400

    same as previous

---
+++ COP2047/Assignmeent 1/Bernstein_HelloWorld.py
@@ -0,0 +1,12 @@
+#Author Name: Aiden Bernstein
+#Date: 08/27/2022
+#Program Name: Bernstein_HelloWorld.py
+#Purpose: Display Hello World
+
+def main():
+    #print a message 5 times
+    for x in range(5):
+        print('Hello World')
+        
+#Call the main function
+main()
